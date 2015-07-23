f9c69a421df592e093c2991bf58b83a848a63f37
kamarul arifin <kamarul.ai@gmail.com>
2015-07-24 00:59:36 +0800

    fine tuning

--- resources/views/admin/promotion/index.blade.php
+++ resources/views/admin/promotion/index.blade.php
@@ -1,17 +1,49 @@
   @extends('admin.layout')
 
-  @section('content')
- <div class="row">
-            <div class="col-xs-12">
-         
-              
+  @section('pageheader')
+  <h1>
+    {{ $page_title or "Promotion" }}
+    <small>{{ $page_description or null }}</small>
+  </h1>
+  <!-- You can dynamically generate breadcrumbs here -->
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Promotion</a></li>
+    <!-- <li class="active">Here</li> -->
+  </ol>
+  @endsection
 
-              <div class="box">
-                <div class="box-header">
-                  <h3 class="box-title">Data Table With Full Features</h3>
-                </div><!-- /.box-header -->
-                <div class="box-body">
-                  <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table class="table table-bordered table-striped dataTable" id="example1" role="grid" aria-describedby="example1_info">
+  @section('content')
+  <div class="row">
+    <div class="col-xs-12">
+
+
+
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">List All Promotion</h3>
+        </div><!-- /.box-header -->
+        <div class="box-body">
+          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="dataTables_length" id="example1_length">
+                  <label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm">
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                  </select> entries</label>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div id="example1_filter" class="dataTables_filter pull-right">
+                    <label>Search&nbsp;:&nbsp;<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <table class="table table-bordered table-striped dataTable" id="example1" role="grid" aria-describedby="example1_info">
                     <thead>
                       <tr role="row">
                         <th>#</th>
@@ -27,7 +59,7 @@
                     </thead>
                     <tbody>
 
-                    @foreach ($promotions as $promotion )
+                      @foreach ($promotions as $promotion )
                       <tr class="gradeA odd" role="row">
                         <td class="sorting_1">1</td>
                         <td>{{ $promotion->Product->name }}</td>
@@ -40,10 +72,45 @@
                       </tr>
                       @endforeach
                     </tbody>
-                   
-                  </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
-                </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
+
+                  </table>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-5">
+                  <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+                </div>
+                <div class="col-sm-7">
+                  <div class="dataTables_paginate paging_simple_numbers pull-right" id="example1_paginate">
+                    <ul class="pagination">
+                      <li class="paginate_button previous disabled" id="example1_previous">
+                        <a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a>
+                      </li>
+                      <li class="paginate_button active">
+                        <a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a>
+                      </li>
+                      <li class="paginate_button ">
+                        <a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a>
+                      </li>
+                      <li class="paginate_button ">
+                        <a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a>
+                      </li><li class="paginate_button ">
+                      <a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a>
+                    </li><li class="paginate_button ">
+                    <a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a>
+                  </li>
+                  <li class="paginate_button ">
+                    <a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a></li>
+                    <li class="paginate_button next" id="example1_next">
+                      <a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
-@endsection
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div><!-- /.col -->
+  </div>
+  @endsection
